7206cd7833b2e153b55292d5fa52c6f012db1237
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-01 21:10:54 +0530

    login signup added

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(morgan('tiny'))
 connectDB()
 
 //load routes
+const userRouter = require('./routes/users')
+
+app.use('/api/users', userRouter)
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port:${PORT}`)
