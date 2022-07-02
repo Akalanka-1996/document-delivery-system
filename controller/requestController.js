f704ddbf2ce94a3bd0df7f1283df97b931cb5bc1
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-02 12:41:54 +0530

    update request

--- controller/requestController.js
+++ controller/requestController.js
@@ -1,41 +1,64 @@
-const Request = require('../model/request.model')
-const asyncHandler = require('express-async-handler')
+const Request = require("../model/request.model");
+const asyncHandler = require("express-async-handler");
 
 // get all requests
 
 const getRequests = asyncHandler(async (req, res) => {
-    const requests = await Request.find()
-    res.json(requests)
-})
+  const requests = await Request.find();
+  res.json(requests);
+});
 
 // get requests by user
 
 const getRequestByUser = asyncHandler(async (req, res) => {
-    console.log(req.body)
-    const requests = await Request.find({user: req.user._id})
-    res.json(requests)
-})
+  console.log(req.body);
+  const requests = await Request.find({ user: req.user._id });
+  res.json(requests);
+});
 
 // create a request
 
 const createRequest = asyncHandler(async (req, res) => {
+  const { title, description, name, address } = req.body;
 
+  if (!title || !description || !name) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  } else {
+    const request = new Request({
+      user: req.user._id,
+      title,
+      description,
+      name,
+      address,
+    });
 
-    const {title, description, name, address} = req.body
-    
-    if(!title || !description || !name) {
-        res.status(400)
-        throw new Error("Please fill all the fields")
-    } else{
-        const request = new Request({
-            user: req.user._id, title, description, name, address
+    const createdRequest = await request.save();
 
-        })
+    res.status(201).json(createdRequest);
+  }
+});
 
-        const createdRequest = await request.save()
+// update a request
 
-        res.status(201).json(createdRequest)
-    }
-})
+const updateRequest = asyncHandler(async (req, res) => {
+  const { title, description, name, address, status } = req.body;
 
-module.exports = {createRequest, getRequests, getRequestByUser}
+  const request = await Request.findById(req.params.id);
+
+  if (request) {
+    request.title = title
+    request.description = description
+    request.name = name
+    request.address = address
+    request.status = status
+
+    const updatedRequest = await request.save();
+    res.json(updatedRequest);
+  } else {
+    res.status(404);
+    throw new Error("Question not found");
+  }
+});
+
+module.exports = { createRequest, getRequests, getRequestByUser, updateRequest };
--- routes/request.js
+++ routes/request.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
-const {createRequest, getRequests, getRequestByUser} = require('../controller/requestController')
+const {createRequest, getRequests, getRequestByUser, updateRequest} = require('../controller/requestController')
 
 const {protect} = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getRequests)
 router.route('/my-requests').get(protect, getRequestByUser)
 router.route('/create').post(protect, createRequest)
+router.route('/:id').put(protect, updateRequest)
 
 module.exports = router
