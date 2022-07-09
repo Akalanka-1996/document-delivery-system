46ca2178c0f7e135a5fcedf1c397179c1e68a58e
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-09 16:14:44 +0530

    register screen

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,15 +10,22 @@ const Login = () => {
   
   });
 
-const onChange = () => {
-
-}
-
-const onSubmit = () => {
-  
-}
-
   const { email, password } = formData;
+
+const onChange = (e) => {
+  setFormData((prevState) => ({
+    ...prevState,
+    [e.target.name]: e.target.value,
+  }));
+}
+
+const onSubmit = (e) => {
+  e.preventDefault();
+
+  console.log(formData)
+}
+
+
 
   return (
     <>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { FaUser } from "react-icons/fa";
 
-const Register = () => {
+function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    studentId: "",
+    pic: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, studentId, pic, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>Register</div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="studentId"
+              name="studentId"
+              value={studentId}
+              placeholder="Student ID"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="pic"
+              name="pic"
+              value={pic}
+              placeholder="Photograph"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
