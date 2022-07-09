c5ce16e9ba7364bc604707237220e4ecdc82106f
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-09 19:12:00 +0530

    login functionality addded

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_URL = '/api/users'
+const API_URL = '/api/users/'
+
+// register user
 
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData)
@@ -13,8 +15,21 @@ const register = async (userData) => {
 }
 
 
+// Login user
+
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
 const authService = {
-    register
+    register,
+    login
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,31 +1,52 @@
 import React from "react";
 import { FaSignInAlt } from "react-icons/fa";
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
+import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from "react-redux";
+import {login, reset} from '../features/auth/authSlice'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-  
   });
 
   const { email, password } = formData;
 
-const onChange = (e) => {
-  setFormData((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value,
-  }));
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-const onSubmit = (e) => {
-  e.preventDefault();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
 
-  console.log(formData)
-}
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
 
+    if (isSuccess || user) {
+      navigate('/')
+    }
 
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {email, password}
+
+    dispatch(login(userData))
+  };
 
   return (
     <>
