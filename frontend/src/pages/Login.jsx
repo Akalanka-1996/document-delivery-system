337acb24f01d0e8f97614eb1c0d9d906c42da254
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-07-09 15:30:09 +0530

    login screen

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { useState, useEffect } from 'react'
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  
+  });
+
+const onChange = () => {
+
 }
 
-export default Login
+const onSubmit = () => {
+  
+}
+
+  const { email, password } = formData;
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <secton className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </secton>
+    </>
+  );
+};
+
+export default Login;
